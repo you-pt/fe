9cb375fc960289c6a3c8488b9d7841379ec2a426
jindonglee <dropnowon@gmail.com>
2024-04-25 20:27:37 +0900

    [F-feat] AI 평가 표 출력 채팅기능 통합

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,7 @@ import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 
 import axios from 'axios'
+import ChatComponent from "./components/ChatComponent";
 
 axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
 // axios.defaults.withCredentials = true
@@ -25,6 +26,7 @@ root.render(
           <Route path="/live" element={<LiveSession/>}/>
           {/* <Route path="/login" element={<Login />} /> */}
           <Route path = "/list" element={<SessionListComponent/>}/>
+          <Route path = "/chat" element = {<ChatComponent/>}/>
           <Route path="/live/:sessionId?" element={<LiveSession/>}/>
           <Route path="/signup" element={<SignUp />} />
           <Route path="/signin" element={<SignIn />} />
