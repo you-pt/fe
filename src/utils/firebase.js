189eb27346ea8c6dc35a71e2173a0b30913c1521
lyukidon <qgx99098@gmail.com>
2024-04-29 12:54:12 +0900

    [F-Fix] 환경변수 문제

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,8 +3,6 @@ import { getMessaging, getToken, onMessage } from "firebase/messaging";
 import firebase from 'firebase/compat/app';
 import 'firebase/messaging';
 
-console.log(process.env)
-
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -12,7 +10,7 @@ const firebaseConfig = {
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_APP_ID
-  };
+};
 
 initializeApp(firebaseConfig);
 
